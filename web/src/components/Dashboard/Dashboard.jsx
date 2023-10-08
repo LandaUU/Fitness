04498a9fcb92a8aa3fadf665d8c3fb0df980689a
Jakov Waimer <yakov.05@mail.ru>
2023-10-08 23:40:17 +0500

    Продвигаемся в верстке!

--- web/src/components/Dashboard/Dashboard.jsx
+++ web/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,41 @@
+import { useLoaderData } from "react-router-dom";
+import { Accordion, AccordionItem, Grid, Column } from "@carbon/react";
+import { useEffect } from "react";
+import { Restaurant } from "@carbon/icons-react";
+
 const Dashboard = () => {
-    return <div className="dashboard-label">My awesome dashboard</div>;
-  };
-  
-  export default Dashboard;
-  
+  const food = useLoaderData();
+
+  const meal_names = [...new Set(food.map((element) => element.meal_name))];
+
+  useEffect(() => {}, []);
+
+  return (
+    <Grid fullWidth>
+      <Column lg={16} md={8} sm={4}>
+        <Accordion>
+          {meal_names.map((meal) => {
+            let my_food = food.filter((f) => f.meal_name == meal);
+            return (
+              <AccordionItem
+                title={
+                  <span>
+                    <Restaurant />
+                    <text>{meal}</text>
+                  </span>
+                }
+                key={meal}
+              >
+                {my_food.map((element) => {
+                  return <div key={element.id}>{element.food_name}</div>;
+                })}
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </Column>
+    </Grid>
+  );
+};
+
+export default Dashboard;
